e02935d28e451946e7694788745993ffb6ddbf92
Sachin Tyagi <sachintyagi6322@gmail.com>
2026-03-25 21:10:52 +0530

    Update auth.controller.js
    Signed-off-by: Sachin Tyagi <sachintyagi6322@gmail.com>

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -15,7 +15,7 @@ export const sighUp=async (req,res) => {
         res.cookie("token",token,{
             httpOnly:true,
             // secure:process.env.NODE_ENVIRONMENT = "production",
-            secure:true;
+            secure:true,
             // sameSite: "strict",
             sameSite: "none",
         
@@ -45,7 +45,7 @@ export const login = async (req,res) => {
         res.cookie("token",token,{
             httpOnly:true,
             // secure:process.env.NODE_ENVIRONMENT = "production",
-            secure:true;
+            secure:true,
             sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
 
